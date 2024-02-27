9a6429d736880056065ba25d977471044ee4c9ef
cabbageflower <naniaaulia84@smk.belajar.id>
2024-02-27 14:44:44 +0700

    commit login

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    public function showLoginForm() {
+        return view('login');
+    }
+
+    public function login (Request $request) {
+        $credentials = $request->only('email', 'password');
+
+        if(Auth::attempt($credentials)) {
+            return redirect()->intended('/dashboard');
+        }
+        return back()->withErrors(['email' => 'Email atau password salah']);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+use App\Http\Controllers\LoginController;
+
+Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [LoginController::class, 'login']);
