db96d2f5f1f0f3cce67d6637517bfd52dc6067da
naniaprima <159672664+cabbageflower@users.noreply.github.com>
2024-03-27 10:37:35 +0700

    perbaikan pada halaman home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,26 +1,26 @@
 @extends('components.navbar-footer')
 
 @section('content')
-    {{-- banner start --}}
-    <div class="bg-slate-300 h-80 mb-8">
-    </div>
-
-    {{-- banner end --}}
+    @include('components.carousel')
 
     {{-- kategori start --}}
-    <div class="bg-white border shadow mb-4 pt-4 w-11/12 mx-auto">
+    <div class="w-11/12 pt-4 mx-auto mb-4 bg-white border shadow">
         <div class="mb-4">
-            <p class="text-center">Kategori Produk</p>
+            <p class="block mb-5 text-xl ml-4">Kategori</p>
         </div>
         <div class="grid grid-cols-4 md:grid-cols-6 lg:grid-cols-8">
-            @for ($i = 0; $i < 16; $i++)
-                <div class="border hover:border-pastel-blue p-4 w-auto cursor-pointer">
-                    <div class="w-10/12 aspect-square bg-primary rounded-full mx-auto">
-                        <img src="" alt="">
+            @foreach ($kategori as $kategori)
+                <div class="relative w-auto p-4 border cursor-pointer hover:border-pastel-blue">
+                    <div class="flex items-center w-10/12 mx-auto rounded-full aspect-square">
+                        <img src="{{ asset('assets/kategori/' . $kategori->kategori . '.jpeg') }}" alt=""
+                            class="w-full h-auto">
                     </div>
-                    <p class=" text-center">Kategori</p>
+                    <p class="text-center ">{{ $kategori->kategori }}</p>
+                    <a href="{{ route('kategori', ['kategori' => $kategori->kategori]) }}"
+                        class="absolute top-0 left-0 w-full h-full"></a>
                 </div>
-            @endfor
+            @endforeach
+
 
 
         </div>
@@ -28,26 +28,26 @@
     {{-- kategori end --}}
 
     {{-- produk start --}}
-    <div class="bg-white  shadow pt-5 w-11/12 flex mx-auto flex-col mb-8 border">
-        <p class=" block  text-center text-4xl mb-5 " style="font-family:'Kaushan Script';">Top Produk</p>
-        <div class=" grid grid-cols-2 sm:grid-cols-4 md:grid-cols-4 lg:grid-cols-6 gap-2 lg:gap-0 ">
+    <div class="flex flex-col w-11/12 pt-5 mx-auto mb-8 bg-white border shadow">
+        <p class="block mb-5 text-4xl text-center ">Top Produk</p>
+        <div class="grid grid-cols-2 gap-2 sm:grid-cols-4 md:grid-cols-4 lg:grid-cols-6 lg:gap-0">
 
             @for ($i = 0; $i < 12; $i++)
                 {{-- card produk --}}
-                <div class=" bg-white  px-1 pb-3 pt-1 border hover:border-blue-700">
+                <div class="relative px-1 pt-1 pb-3 bg-white border hover:border-blue-700">
                     <div class=" aspect-[1.2/1] bg-slate-400 w-full  ">
                         <img src="" alt="">
                     </div>
                     <div class="px-2">
                         <div class="mb-10">
-                            <a href="{{ url('/produk/' . $i) }}"
-                                class="text-lg hover:text-sky-700 hover:underline duration-100">Contoh
-                                Produk</a>
-                            <p class="text-slate-500 text-sm">Rp 999 999 999</p>
+                            <p class="text-lg duration-100 hover:text-sky-700 hover:underline">Contoh
+                                Produk</p>
+                            <p class="text-sm text-slate-500">Rp 999 999 999</p>
                             <p>Kategori</p>
                         </div>
                         <p class="text-sm">XX Terjual</p>
                     </div>
+                    <a href="{{ url('/produk/' . $i) }}" class="absolute top-0 left-0 w-full h-full"></a>
                 </div>
                 {{-- card produk --}}
             @endfor
